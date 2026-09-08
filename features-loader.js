63cc5d333ad7f917a99c230b0d4fedaf91795361
Dyyjoao <114023056+Dyyjoao@users.noreply.github.com>
2026-09-08 17:24:50 -0300

    feat: load credit card payment reversal controls

--- features-loader.js
+++ features-loader.js
@@ -2,6 +2,7 @@ const style=document.createElement('link');style.rel='stylesheet';style.href='./
 Promise.all([
  import('./ofx-enhancements.js'),
  import('./cards-enhancements.js'),
+ import('./cards-reversal-enhancements.js'),
  import('./investments-enhancements.js'),
  import('./reserves-enhancements.js'),
  import('./accounts-enhancements.js'),
